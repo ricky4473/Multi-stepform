f8bcc6ed5bd6fb2287235b4439b61e9a4112399c
ricky4473 <146510506+ricky4473@users.noreply.github.com>
2024-04-22 11:37:39 +0000

    toggle bug

--- app/select-plan/page.tsx
+++ app/select-plan/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useRef, useState } from "react";
 import Image from "next/image";
 import iconarcade from "@/public/images/icon-arcade.svg";
 import iconadvanced from "@/public/images/icon-advanced.svg";
@@ -9,6 +11,8 @@ const plan = [
   { image: iconpro, title: "Pro", price: 15 },
 ];
 export default function () {
+  const [selectedPlan, setSelectedPlan] = useState("Arcade");
+  const [monthYear, setMonthYear] = useState(true);
   return (
     <div class="var">
       <h1 class="mt-1 text-xl font-semibold">Select your plan</h1>
@@ -19,6 +23,8 @@ export default function () {
         {plan.map((item, index) => {
           return (
             <PlanButton
+              selectedPlan={selectedPlan}
+              setSelectedPlan={setSelectedPlan}
               key={index}
               image={item.image}
               title={item.title}
@@ -26,6 +32,30 @@ export default function () {
             />
           );
         })}
+      </div>
+      <div className="mt-3 p-3 flex flex-row justify-center items-center bg-blue-100 rounded-lg">
+        <p
+          class={`ms-3 text-sm font-medium  dark:text-gray-300 mr-2 ${monthYear ? "text-gray-900" : "text-slate-300"}`}
+        >
+          Monthly
+        </p>
+        <label class="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            class="sr-only peer"
+            checked
+            onChange={(e) => {
+              setMonthYear(e.target.value);
+            }}
+          />
+          <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        </label>{" "}
+        <p
+          class={`ms-3 text-sm font-medium  dark:text-gray-300 mr-2 ${!monthYear ? "text-gray-900" : "text-slate-300"}`}
+        >
+          Yearly
+        </p>
       </div>
     </div>
   );
--- components/PlanButton.tsx
+++ components/PlanButton.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
-export default function ({ image, title, price }) {
+
+export default function ({
+  image,
+  title,
+  price,
+  selectedPlan,
+  setSelectedPlan,
+}) {
   return (
-    <button className="border border-slate-300 w-full rounded-lg  h-[140px] p-3 flex flex-col justify-between items-start">
+    <button
+      onClick={() => setSelectedPlan(title)}
+      className={`border border-slate-300 hover:border-blue-500 w-full rounded-lg  h-[140px] p-3 flex flex-col justify-between items-start ${selectedPlan === title ? "bg-blue-100" : ""}`}
+    >
       <Image src={image}></Image>
       <div className="text-left">
-        <h1>{title}</h1>
-        <p>${price}/mo</p>
+        <h1 class=" font-bold">{title}</h1>
+        <p class="text-xs text-slate-400">${price}/mo</p>
       </div>
     </button>
   );
